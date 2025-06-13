45d71ba5b5589aab761111fa56a4e103affa2356
toogle <toogletoogle110@gmail.com>
2025-06-13 11:14:47 +0800

    更新

--- senceObject.js
+++ senceObject.js
@@ -8,8 +8,7 @@ export const spheres = [
 export const boxes = [
     { index:-1, center: [-5.0, 0.0, 0.0], size: [2.0, 2.0, 2.0], color:[0.0, 0.0, 1.0], reflectivity:0.1, type:1, ior:0 },
     { index:-1, center: [0.0, 0.0, -8.0], size: [10.0, 10.0, 0.1], color:[0.05, 0.05, 0.05], reflectivity:0.9, type:2, ior:0 },
-    { index:-1, center: [0.0, -10.0, 0.0], size: [10.0, 0.1, 10.0], color:[1.0, 1.0, 1.0], reflectivity:0.001, type:1, ior:0 },
-    { index:-1, center: [-10.0, 10.0, -5.0], size: [5.0, 5.0, 20.0], color:[0.0, 0.0, 0.0], reflectivity:0.001, type:1, ior:0 }
+    { index:-1, center: [0.0, -10.0, 0.0], size: [10.0, 0.1, 10.0], color:[1.0, 1.0, 1.0], reflectivity:0.001, type:1, ior:0 }
 ];
 
 export const pyramids = [
